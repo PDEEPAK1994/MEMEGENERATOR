e4abf3138588b3e67a91c0f94469487e0c415f9a
PDEEPAK1994 <pd13604@gmail.com>
2016-04-30 12:11:43 +0530

    Initial commit

---
+++ app/src/androidTest/java/com/example/prashant/memegenerator/BottomPictureFragmentTest.java
@@ -0,0 +1,10 @@
+package com.example.prashant.memegenerator;
+
+import junit.framework.TestCase;
+import android.os.Bundle;
+/**
+ * Created by Prashant on 24-04-2016.
+ */
+public class BottomPictureFragmentTest extends TestCase {
+
+}
---
+++ app/src/main/java/com/example/prashant/memegenerator/BottomPictureFragment.java
@@ -0,0 +1,36 @@
+package com.example.prashant.memegenerator;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Prashant on 24-04-2016.
+ */
+public class BottomPictureFragment extends Fragment {
+
+    private static TextView topMemeText;
+    private static TextView bottomMemeText;
+
+    public View onCreateView(LayoutInflater inflater,@Nullable ViewGroup container,@Nullable Bundle savedInstanceState ){
+        View view=inflater.inflate(R.layout.bottom_picture_fragment,container , false);
+        topMemeText=(TextView) view.findViewById(R.id.topMemeText);
+        bottomMemeText=(TextView) view.findViewById(R.id.bottomMemeText);
+
+        return  view;
+    }
+
+
+
+
+
+    public void setMemeText(String top,String bottom)
+    {
+        topMemeText.setText(top);
+        bottomMemeText.setText(bottom);
+    }
+}
